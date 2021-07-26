88478c1f0641a3b400f09e8366d38f20c37bb1f0
roman.gradyskiy <roman.gradyskiy@skylum.com>
2021-07-26 12:10:22 +0300

    stress test

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/make-fake-data', function (Request $request) {
+    \App\Models\Product::factory()->count(2000)->create();
+    return response()->json(['status' => 'ok']);
+});
+
+Route::get('/delete-fake-data', function (Request $request) {
+    \App\Models\Product::query()->delete();
+    return response()->json(['status' => 'ok']);
+});
---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/products', function () {
+    $products = \App\Models\Product::all()->sortBy(['created_at', 'updated_at']);
+
+    return view('products', ['products' => $products]);
+});
+
+Route::get('/products-cache', function () {
+    $products = Cache::get('products');
+    $statusCode = 304;
+    if (empty($products)) {
+        $products = \App\Models\Product::all()->sortBy(['created_at', 'price', 'updated_at']);
+        Cache::put('products', $products, 7);
+        $statusCode = 200;
+    }
+
+    return response()->view('products', ['products' => $products], $statusCode);
+});
+
+Route::get('/products-cache-stampede', function () {
+    $productsTtl = Cache::get('products-ttl');
+    $statusCode = 304;
+    if (!$productsTtl || $productsTtl < time()) {
+        Cache::put('products-ttl', strtotime("+7 seconds"), 15);
+        $products = \App\Models\Product::all()->sortBy(['created_at', 'updated_at']);
+        Cache::put('products', $products, 7);
+        $statusCode = 200;
+    } else {
+        $products = Cache::get('products');
+    }
+
+    return response()->view('products', ['products' => $products], $statusCode);
+});
+
+Route::get('/products-cache-exp', function () {
+    $productsTtl = Cache::get('products-ttl');
+    $statusCode = 304;
+    $exp = random_int(30, 100) * exp(0.3 / ($productsTtl - time() + 0.1)) / 100;
+    if (!$productsTtl || $exp > 1) {
+        Cache::put('products-ttl', strtotime("+7 seconds"), 7);
+        $products = \App\Models\Product::all()->sortBy(['created_at', 'price', 'updated_at']);
+        Cache::put('products', $products, 7);
+        $statusCode = 200;
+    } else {
+        $products = Cache::get('products');
+    }
+
+    return response()->view('products', ['products' => $products], $statusCode);
+});
